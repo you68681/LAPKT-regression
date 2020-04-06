860830d6a03e72f7a93bae65597b452e8d06fa79
Nir <nirlipo@gmail.com>
2020-04-06 12:49:51 +1000

    Chao Fixed mutex computation

--- LAPKT-public/interfaces/agnostic/h_2.hxx
+++ LAPKT-public/interfaces/agnostic/h_2.hxx
@@ -701,13 +701,9 @@ protected:
 
                                     }
                                 }
-                                /**chao add
-                                 *  if  anyone of the r,s pair can not be set to 0 when consider the relevant_actions, we should break.
-                                 */
-                                 if (h2_pre_noop == infty && value(r,s)==infty){
-                                     break;
-                                 }
-
+                                
+                                //if anyone of the r,s pair can not be set to 0 when consider the relevant_actions, we should break.
+                                if (h2_pre_noop == infty )  break;
 
                             }
 						}
